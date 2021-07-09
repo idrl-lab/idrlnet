d1598bf8281419da4e1e233aae7bdd6863e8453f
weipengOO98 <weipeng0098@hotmail.com>
2021-07-09 08:11:43 +0800

    docs: Update version number

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, IDRL'
 author = 'IDRL'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.4'
+release = '0.0.1-alpha'
 
 # -- General configuration ---------------------------------------------------
 
