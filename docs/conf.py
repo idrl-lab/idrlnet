84cbcce755cfd0a82a32e55bcff07292d6b1f8ef
zweien <278954153@qq.com>
2021-07-12 15:37:03 +0800

    chroe: add bump2version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, IDRL'
 author = 'IDRL'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1-alpha'
+release = '0.0.1-rc1'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="idrlnet",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.1-rc1",
     author="Intelligent Design & Robust Learning lab",
     author_email="weipeng@deepinfar.cn",
     description="IDRLnet",
