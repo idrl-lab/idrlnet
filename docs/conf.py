d8972abcc7960cfcfe76904f743efe66e88b8e57
zweien <278954153@qq.com>
2021-07-15 13:09:29 +0800

    Bump version: 0.0.1-rc1 → 0.0.1-rc2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, IDRL"
 author = "IDRL"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1-rc1"
+release = "0.0.1-rc2"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def load_requirements(path_dir=here, comment_char="#"):
 
 setuptools.setup(
     name="idrlnet",  # Replace with your own username
-    version="0.0.1-rc1",
+    version="0.0.1-rc2",
     author="Intelligent Design & Robust Learning lab",
     author_email="weipeng@deepinfar.cn",
     description="IDRLnet",
