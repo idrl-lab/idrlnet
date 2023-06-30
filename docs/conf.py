ceaf7e29079af30806ace4e615981178187971c6
weipengOO98 <weipeng0098@hotmail.com>
2023-06-30 14:25:43 +0800

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, IDRL"
 author = "IDRL"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.2-rc3"
+release = "2.0.0-rc3"
 
 # -- General configuration ---------------------------------------------------
 
